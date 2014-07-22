ce8962ba98138a4409f4238ece0dd888b8df8eb7
ahilles107 <mikolajczuk.private@gmail.com>
2014-07-22 13:02:59 +0200

    add license

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Adverts Plugin.
+ *
+ * (c) Paweł Mikołajczuk <mikolajczuk.private@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace AHS\AdvertsPluginBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
--- EventListener/LifecycleSubscriber.php
+++ EventListener/LifecycleSubscriber.php
@@ -1,4 +1,14 @@
 <?php
+
+/*
+ * This file is part of the Adverts Plugin.
+ *
+ * (c) Paweł Mikołajczuk <mikolajczuk.private@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 /**
  * @package Newscoop\ExamplePluginBundle
  * @author Paweł Mikołajczuk <pawel.mikolajczuk@sourcefabric.org>
